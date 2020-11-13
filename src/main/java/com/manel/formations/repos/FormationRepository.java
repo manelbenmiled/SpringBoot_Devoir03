e65d5a0e52873fdf3336d4bd275e7cfcce400762
manelbenmiled <72624490+manelbenmiled@users.noreply.github.com>
2020-11-13 15:39:04 +0100

    devoir04 tp5

--- src/main/java/com/manel/formations/repos/FormationRepository.java
+++ src/main/java/com/manel/formations/repos/FormationRepository.java
@@ -5,10 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.manel.formations.entities.Centre;
 import com.manel.formations.entities.Formation;
 
+@RepositoryRestResource(path="rest")
 public interface FormationRepository extends JpaRepository<Formation, Long> {
 
 	List<Formation> findByNomFormation(String nomFormation);
